7dc155b8281c53fd1ac5d2a10241a9b19f9ce88c
Federico Russo <russo.federico@anteash.com>
2012-10-08 16:35:59 +0200

    Removed the stuff from the tutorial.

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
 var express = require("express");
-var formidable = require("formidable");
-var fs = require("fs");
 var jade = require("jade");
 var path = require("path");
 var http = require("http");
@@ -20,45 +18,6 @@ db.run('create table items (name text, expires datetime)', function() {
                 }
         });
 });
-
-function start(request, response) {
-        console.log("Request handler 'start' was called.");
-        response.render('start.jade');
-}
-
-function upload(request, response) {
-        console.log("Request handler 'upload' was called.");
-        console.log('About to parse...');
-        var form = new formidable.IncomingForm();
-        form.parse(request, function(error, fields, files) {
-                console.log('Parsing done.');
-                fs.rename(files.upload.path, "/tmp/test.png", function(err) {
-                        if(err) {
-                                fs.unlink("/tmp/test.png");
-                                fs.rename(files.upload.path, "/tmp/test.png");
-                        }
-                });
-                response.render('upload.jade');
-        });
-}
-
-function show(request, response) {
-        console.log("Request handler 'show' was called.");
-        fs.readFile("/tmp/test.png", "binary", function(error, file) {
-                if(error) {
-                        response.writeHead(500, {
-                                "Content-Type": "text/plain"
-                        });
-                        response.write(err + "\n");
-                } else {
-                        response.writeHead(200, {
-                                "Content-Type": "image/png"
-                        });
-                        response.write(file, "binary");
-                }
-                response.end();
-        });
-}
 
 function add(request, response) {
         var name = request.param('name', null);
@@ -115,10 +74,6 @@ app.get('/', list);
 app.get('/list', list);
 app.post('/add', add);
 app.get('/del', del);
-// From the tutorial
-app.get('/start', start);
-app.post('/upload', upload);
-app.get('/show', show);
 
 http.createServer(app).listen(app.get('port'), function() {
         console.log("ExpiredJS is listening on port " + app.get('port') + " (with express).")
